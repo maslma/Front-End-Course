ee08fc9c392c14c6cac250e251c73ecebfee28a7
Eng-Maslma <maslmaalzebda23@gmail.com>
2025-03-10 12:12:21 +0200

    Add new lecture-39 and edit lecture-38

---
+++ Lecture/lecture-38/index.js
@@ -0,0 +1,86 @@
+// OOP: Object-Oriented Programming
+// code style/pattern based on classes/objects.
+// constructor functions or classes: blue-print for creating objects.
+// classes: syntax sugar for constructor functions with some differences or additional features. in classes we use strict mode by default.
+// extends keyword does prototype inheritance for us.
+// classes consists of properties (variables) and methods (functions).
+// constructor() is a special method gets called when we create an instance of a class.
+// super() is for using parent class constructor and methods (this keyword for parent class).
+// public, private, protected
+// public: accessible from anywhere (class, its sub-classes, and its instances).
+// protected: accessible from class and its sub-classes (no enforcement from language-level) - (underscore syntax).
+// private: accessible only from class (hashtag syntax).
+
+class Counter {
+  static name = "Counter";
+
+  #value = 0; // private-> مسموح نستخدموا بس بنفس الكلاس
+
+  constructor(value) {
+    this.#value = value;
+  }
+
+  #validateValue(value) {
+    if (typeof value !== "number" || value < 0) return false;
+    return true;
+  }
+
+  increment(value) {
+    if (!this.#validateValue(value)) return;
+    this.#value += value;
+  }
+
+  decrement(value) {
+    if (!this.#validateValue(value)) return;
+    this.#value -= value;
+  }
+
+  reset() {
+    this.#value = 0;
+  }
+
+  get value() {
+    return this.#value;
+  }
+}
+
+const counter = new Counter(1);
+
+counter.increment(10);
+counter.decrement(5);
+
+counter.reset();
+counter.increment(5);
+
+console.log(counter.value);
+
+class CounterB extends Counter {
+  constructor(value) {
+    super(value);
+  }
+}
+
+class CounterC {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+const counterB = new CounterB(1);
+const counterC = new CounterC(1);
+
+counterB.reset();
+counterB.increment(3);
+
+console.log(counterB.value);
+
+// بدنا نعرف هاد الكلاس انعمل من كلاس معين ولا لاء بمعنى يرث من كلاس
+console.log(counter instanceof Counter);
+
+console.log(counterB instanceof Counter);
+console.log(counterB instanceof CounterB);
+
+console.log(counterC instanceof Counter);
+console.log(counterC instanceof CounterC);
+
+console.log(Counter.name);
---
+++ Lecture/lecture-39/index.js
@@ -0,0 +1,195 @@
+console.log("-----------Error and ThrowsError--------");
+// Error and ThrowsError
+// console.log(x);
+try {
+  console.log(y);
+} catch (error) {
+  console.log("Name:", error.name); // ReferenceError -> مش موجود بالذاكرة
+  console.log(" Message:", error.message); // y is not defined ->
+  console.log("Cause:", error.cause); // cause -> السبب
+  console.log("Stack:", error.stack); // كيف ممكن اتبع الايرور زي موقع الايرور
+}
+
+function MyErrorF(message) {
+  // function body
+  this.message = message;
+}
+
+class MyErrorC {
+  constructor(message) {
+    this.message = message;
+  }
+}
+
+new MyErrorC("Uncaught ReferenceError: x is not defined");
+// Built-in Classes -> مبنية من اللغة
+// هان عملنا instance من الكلاس Error
+const err = new Error("Uncaught ReferenceError: x is not defined");
+
+console.log(err instanceof Error); // true
+console.log(typeof err); // object
+console.log(err);
+console.log(err.message);
+
+// Throw Error -> (retrun Error) تبع البرنامج Exection الخاص بالخطأ بتوقف
+
+function returnSometing() {
+  console.log("1."); // يطبع
+  return "Something"; // يتوقف البرنامج
+  console.log("2.");
+}
+
+const result = returnSometing();
+console.log(result);
+// throw error;
+// console.log("3.");
+
+console.log("------------ Error Handling ----------");
+// Error Handling -> بدي اعرف اتعامل مع الايرور
+function isAuthenticated(user) {
+  if (!user) {
+    throw new Error("User is not authenticated");
+  }
+
+  if (!user.isAuthenticated) {
+    throw new Error(`${user.name} is not authenticated`);
+  }
+
+  return `${user.name} is authenticated`;
+}
+
+// Imagine this is our users form our database
+const users = [
+  { name: "Maslma", isAuthenticated: true },
+  { name: "waseem", isAuthenticated: true },
+  undefined,
+  { name: "Ahmed", isAuthenticated: false },
+];
+
+// forEch -> loop of array -> undefined بترجع
+users.forEach((user) => {
+  // finally-> Loading من اشهر الاستخدامات الها نعمل
+  console.log("Loading started...");
+  try {
+    // try: to execute a code that may throw an error.
+    // بنحط الايرور الي متوقع يطلع بس بدناش نوقف البرنامج
+    const result = isAuthenticated(user);
+    console.log(result);
+  } catch (error) {
+    // catch: to handle errors from try block   .
+    // هان لكود الي بدو يسير بدل ما نوقف الكود
+    console.warn(
+      "Hi user! We are sorry to tell you that your action did Something wrong With our website! Here are more details about error:"
+    );
+    console.error("Erorr:", error.message);
+  } finally {
+    // console نعطي معلومة في
+    console.info(`${users.name} has been processed`);
+    console.log("Loading finished!...");
+  }
+});
+
+console.log("The program is still running");
+
+console.log("----------- Fetching... ----------");
+// Example From Real World (Log-in Form):
+// 1- Sometimes you will need custom errors. 
+// So you can create your own error class.
+
+// FetchError -> عارف راح اتعامل مع سيرفر
+// هيك بنعمل custome error
+class FetchError extends Error {
+  status;
+
+  statusText;
+
+  constructor(status, statusText, message, cause) {
+    super(message, { cause }); // new Error(message, {cause})
+    this.name = "FetchError";
+    this.status = status;
+    this.statusText = statusText;
+  }
+}
+
+function fetchSomething(request) {
+  if (request === "users" || request === "setting")
+    return "Response: Users or Setting";
+
+  // throw new FetchError(
+  //   404,
+  //   "Not Found",
+  //   "What you have requseted dose not exist on our server!",
+  //   "Stupid user!"
+  // );
+
+  // FetchError هان بدل ما نستخدم
+  throw Error("What you have requseted dose not exist on our server!");
+}
+// Use Error Handling
+try {
+  // const response = fetchSomething(prompt("Enter your request: "));
+  const response = fetchSomething("users");
+  console.log(response);
+} catch (error) {
+  if (error instanceof FetchError) {
+    console.warn(" Status Text:", error.statusText);
+    console.error(" Message:", error.message);
+  } else {
+    console.error(" Message:", error.message);
+  }
+}
+console.log("--------- Fetching One and Two -------------");
+
+// ReThrowing هاد عبارة عن  ErrorHandling هاد مش  catch جو throw لما نشوف 
+const requests = {
+  users: { response: "...some users" },
+  settings: { response: "...some settings" },
+};
+
+function fetchOneThing(request) {
+  try {
+    const response = request[request].response;
+    return response;
+  } catch (error) {
+    throw new FetchError(
+      404,
+      "Not Found",
+      `What you have requseted (${request}) dose not exist on our server!`,
+      error.cause
+    );
+  } finally {
+    console.log("Fetching one thing is finished!");
+  }
+}
+
+function fetchManyThings(requests) {
+  const responses = [];
+  for (const request of requests) {
+    try {
+      const response = fetchOneThing(request);
+      responses.push(response);
+    } catch (error) {
+      // rethrowing -> بدي اعالجو برا fetchManyThings ما بدي اعالجو في
+      throw error;
+    } finally {
+      console.log("Fetching many thing is finished!");
+    }
+    return responses;
+  }
+}
+// Try and Catch بشكل عام هاد الكود ممكن يطلع ايرور وما بدي الايرور هاد يوقف البرنامج تبعي بستخدم
+try {
+  const responses = fetchManyThings(["users", "settings", "blogs"]);
+  console.log(responses);
+} catch (error) {
+  if (error instanceof FetchError) {
+    console.warn(" Status Text:", error.statusText);
+    console.error(" Message:", error.message);
+  } else {
+    console.error(" Message:", error.message);
+  }
+} finally {
+  console.log("Finished!");
+}
+
+console.log("----------- Promises and async/await -----------");
